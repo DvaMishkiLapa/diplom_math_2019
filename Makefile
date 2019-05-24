9c9de352e05d7ffe6e349486a214d937dfccd6d1
DvaMishkiLapa <artem132rus@gmail.com>
2019-05-25 01:50:20 +0300

    Added pulse graph

--- Makefile
+++ Makefile
@@ -2,7 +2,9 @@ SHELL=/bin/bash
 DOC=diplom
 
 all:
+	python3 ./grap/grap_gen.py
 	latexmk -synctex=1 -interaction=nonstopmode -file-line-error -pdf -outdir=./ $(DOC).tex
 
 clean:
-	rm *.aux *.log *.out *.toc *.pdf *.fls *.gz *.fdb_latexmk
+	rm ./grap/*.pgf ./grap/*.pdf
+	rm *.aux *.log *.out *.toc *.pdf *.fls *.gz *_latexmk
---
+++ grap/grap_gen.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import math
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def impulse():
+    x = [x * 0.0005 for x in range(-int(math.pi / 0.0005), int(math.pi / 0.0005))]
+    y = []
+    count = 3
+
+    for X in x:
+        summa = []
+        for j in range(0, count):
+            summa.append((count - j) / count * math.cos((j + 1) * X))
+        y.append(sum(summa))
+        summa.clear()
+
+    plt.figure(figsize=(5.8, 3.0))
+    plt.plot(x, y, linewidth=2)
+    plt.grid()
+
+    plt.savefig('./grap/impulse.pdf')
+    plt.savefig('./grap/impulse.pgf')
+
+
+impulse()
